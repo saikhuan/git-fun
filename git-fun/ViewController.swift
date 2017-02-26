70a71e2aad11336323014f897961f51debb9bfc7
Sai Khuan <saikhuan@SKs-MacBook-Pro.local>
2017-02-26 22:48:44 +0800

    First Commit

---
+++ git-fun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  git-fun
+//
+//  Created by Sai Khuan on 2/26/17.
+//  Copyright © 2017 saikhuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		print("HEllow Worl")
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
